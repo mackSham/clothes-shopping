a948a1f06604be4f7eb713a01ab3b68f5f9c1a8d
Mayank Sharma (164891) <maysharm@amdocs.com>
2020-03-22 19:55:14 +0530

    Broken our Homepage into different componet and styled each component

---
+++ src/pages/homepage/hompage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./hompage.styles.scss";
+import Directory from "./../../component/directory/directory.component";
+
+function HomePage() {
+  return (
+    <div className="hompage">
+      <Directory />
+    </div>
+  );
+}
+
+export default HomePage;
