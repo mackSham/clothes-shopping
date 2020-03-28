2f743908654c10704dffa2828f5ffd97f43ab5d9
Mayank Sharma (164891) <maysharm@amdocs.com>
2020-03-28 17:13:54 +0530

    Basic Routing in SPA

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
 
-import HomePage from './pages/homepage/hompage.component';
-
+import HomePage from "./pages/homepage/hompage.component";
+function HatsPage() {
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+}
 function App() {
-  return <div><HomePage></HomePage></div>; 
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route exact path="/hats" component={HatsPage}></Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
